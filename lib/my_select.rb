09c5999e10476f1219d3b23960675ec2b51ae154
Ethan Roberts <ejrobe@gmail.com>
2018-12-31 21:09:16 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ if block_given?
+   x = Array.new
+    for each in collection
+      if yield(each) == true
+        x << each
+      end
+    end
+    
+ end
+ return x
 end
