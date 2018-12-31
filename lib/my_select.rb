36455c3ae1c352c0566a21bf15e5bcd624f2695b
Ethan Roberts <ejrobe@gmail.com>
2018-12-31 20:21:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  if block_given?
    x = Array.new
     for each in collection
-      if yield == true
+      if yield(each) == true
         x << yield
       end
     end
